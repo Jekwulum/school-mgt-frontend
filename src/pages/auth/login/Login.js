9a6474af27c48c5049b9c4397df10aa887fa5712
Charles Nwoye <charlesnwoye2@gmail.com>
2022-09-10 12:05:17 +0100

    added login UI

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
+import './Login.css';
+import 'animate.css';
 
+import imageLogin from '../../../assets/img/imageLogin.jpg'
 import EncryptHelper from '../../../utils/helpers/encryptHelper';
 import TokenHelper from '../../../utils/helpers/tokenHelper';
 import { Loading } from '../../../utils/helpers/constants';
@@ -8,16 +11,22 @@ import AuthService from '../../../utils/services/auth.service';
 
 const Login = () => {
 
-  
+
   const history = useHistory();
   const isLoggedIn = TokenHelper.checkIfLoggedIn();
   const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordType, setPasswordType] = useState("password");
   const [loading, setLoadingStatus] = useState(false);
   const [message, setMessage] = useState("");
   const [errorStatus, setLoginErrorStatus] = useState(false);
 
   if (isLoggedIn) return <Redirect to={{ pathname: '/' }} />;
+
+  const togglePasswordField = type => {
+    setPasswordType(type);
+    document.getElementById('password').type = type;
+  }
 
   const handleLogin = async () => {
     setLoginErrorStatus(false);
@@ -61,19 +70,39 @@ const Login = () => {
 
   return (
     <div>
-      <form>
-        <div>
-          <label>User ID: </label>
-          <input value={userId} placeholder='Enter ID' type="text" onChange={e => setUserId(e.target.value)} />
+      <section className="login py-5 bg-light">
+        <div className="container">
+          <div className="row g-0">
+            <div className="col-lg-5">
+              <img src={imageLogin} className="image-fluid" />
+            </div>
+            <div className="col-lg-7 text-center py-5">
+              <h1 className='animate__animated animate__pulse animate__infinite	infinite'><i>mySchool</i></h1>
+
+              <form>
+                <div className="form-row py-3 pt-5">
+                  <div className="offset-1 col-lg-10">
+                    <input className='inp px-3' type="text" placeholder='Enter ID' />
+                  </div>
+                </div>
+
+                <div className="form-row py-3">
+                  <div className="offset-1 col-lg-10">
+                    <input className='inp px-3' type="password" placeholder='Enter password' />
+                  </div>
+                </div>
+
+                <div className="form-row py-3">
+                  <div className="offset-1 col-lg-10">
+                    <button className='btn1'>Sign In</button>
+                  </div>
+                </div>
+              </form>
+              <p className='forgot-password'>forgot password?</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <label>Password: </label>
-          <input value={password} placeholder='Enter Password' type="password" onChange={e => setPassword(e.target.value)} />
-        </div>
-        <button type='button' onClick={handleLogin}>
-          Sign In{loading ? <>&nbsp;<i className='fa fa-spin fa-spinner' /> </> : ""}
-        </button>
-      </form>
+      </section>
     </div>
   )
 }
